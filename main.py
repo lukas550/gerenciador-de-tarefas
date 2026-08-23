18def5218aa5dffa41072d7d9cb1db84089bab92
Lukas <xd3433660@gmail.com>
2026-08-23 15:42:27 -0300

    feat: adiciona estrutura base do menu principal

---
+++ main.py
@@ -0,0 +1,26 @@
+
+menu = {
+    "1": "Adicionar Tarefa",
+    "2": "Listar Tarefas",
+    "3": "Concluir Tarefa",
+    "4": "Excluir Tarefa",
+    "5": "Sair"
+}
+
+while True:
+
+    escolha = input("Digite o número da opção que deseja: ").strip()
+
+    if escolha == "1":
+        pass
+    elif escolha == "2":
+        pass
+    elif escolha == "3":
+        pass
+    elif escolha == "4":
+        pass
+    elif escolha == "5":
+        print("\nEncerrando...\n")
+        break
+    else:
+        print("\nDigite uma opção válida!\n")
