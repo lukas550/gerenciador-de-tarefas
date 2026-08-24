2ac922511caad7e024d25e4418b579e07db5662b
Lukas <xd3433660@gmail.com>
2026-08-24 17:42:43 -0300

    feat(core): implementa módulo organizacao com funções auxiliares

---
+++ core/organizacao.py
@@ -0,0 +1,11 @@
+# Módulo para a organização do Main
+
+def lin(char, qtd=30):
+    print(char * qtd)
+
+def tabela(menu):
+    if isinstance(menu, dict):
+        for nu, item in menu.items():
+            print(f"{nu}. {menu}")
+    else:
+        print("\nO objeto precisa ser um dicioário!\n")
