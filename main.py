f3ddba166730a4977380aa0993855f4170efbf90
Lukas <xd3433660@gmail.com>
2026-09-08 16:30:28 -0300

    feat: adiciona a opção de cadastrar tarefas no main.py

--- main.py
+++ main.py
@@ -35,7 +35,22 @@ while True:
         lin("-")
 
     elif escolha == "1":
-        pass
+
+        lin("-")
+        try:
+            nome_da_tarefa = input("Digite o nome da tarefa: ")
+            descricao_da_tarefa = input(f"Digite a descrição da tarefa {nome_da_tarefa.capitalize()} (MAX 50 caracteres): ")
+
+            tarefa = adicionar_tarefa(nome_da_tarefa, descricao_da_tarefa)
+        except ValueError as e:
+            print(f"\n{e}\n")
+
+        else:
+            tarefas.append(tarefa)
+            salvar_arquivo(tarefas)
+
+            print(f"\nTarefa {nome_da_tarefa.capitalize()} foi cadastrado!\n")
+        lin("-")
 
     elif escolha == "2":
         pass
