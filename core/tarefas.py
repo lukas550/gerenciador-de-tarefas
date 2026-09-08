88a1a4b38fe978e49a0b28eec41802e67570d6da
Lukas <xd3433660@gmail.com>
2026-09-08 16:15:10 -0300

    refactor(core): refatora o módulo tarefas.py para a persistência JSON

--- core/tarefas.py
+++ core/tarefas.py
@@ -1,13 +1,5 @@
 # Módulo para o CRUD do main.py
 
-"""
-estrutura:
-    {
-        nome_da_tarefa: string
-        descricao: string (max de 50 char)
-        status: bool (true/false)
-    }
-"""
 def adicionar_tarefa(nome, descricao): # Função para adicionar uma tarefa á lista.
     if not nome.strip() or not descricao.strip():
         raise ValueError("Nome ou descrição estão faltando!")
@@ -16,29 +8,41 @@ def adicionar_tarefa(nome, descricao): # Função para adicionar uma tarefa á l
         raise ValueError("Máximo de caracteres em descrição foi atingido!")
 
     return {
-        "nome_da_tarefa": nome,
+        "tarefa": nome.lower().strip(),
         "descricao": descricao,
-        "status": False
+        "concluida": False
     }
 
 def listar_tarefas(tarefas): # lista as tarefas ordenadamente.
     if not tarefas:
         print("\nSem tarefas cadastradas!\n")
     else:
-        for idx, tarefa in enumerate(tarefas, 1):
-            status = "Concluída" if tarefa["status"] else "Não concluída"
+        for t in tarefas:
+            status = "Concluida" if t["concluida"] else "Não concluida"
+            print(f"- {t['tarefa'].capitalize()} - {t['descricao']} - Status: {status}")
 
-            print(f"{idx}. {tarefa['nome_da_tarefa']} | {tarefa['descricao']} | STATUS: {status}")
+def concluir_tarefa(tarefa_a_concluir, tarefas):
+    # Busca
+    encontrado = False
+    for t in tarefas:
+        if t["tarefa"].lower().strip() == tarefa_a_concluir.lower().strip():
+            encontrado = True
+            tarefa_encontrada = t
+            break
+    if not encontrado:
+        raise ValueError(f"\nA tarefa {tarefa_a_concluir.capitalize()} não foi encontrada!\n")
 
-def concluir_tarefa(tarefas, indice):
-    if indice < 0 or indice >= len(tarefas):
-        raise IndexError("Indice inválido!")
-    
-    tarefas[indice]["status"] = True
+    tarefa_encontrada['concluida'] = True
 
-def excluir_tarefa(tarefas, indice):
-    if indice < 0 or indice >= len(tarefas):
-        raise IndexError("Indice inválido!")
+def excluir_tarefa(tarefa_a_excluir, tarefas):
+    # Busca
+    encontrado = False
+    for t in tarefas:
+        if t["tarefa"].lower().strip() == tarefa_a_excluir.lower().strip():
+            encontrado = True
+            tarefa_encontrada = t
+            break
+    if not encontrado:
+        raise ValueError(f"\nA tarefa {tarefa_a_excluir.capitalize()} não foi encontrada!\n")
 
-    tarefa_removida = tarefas.pop(indice)
-    return tarefa_removida
+    return tarefa_encontrada
